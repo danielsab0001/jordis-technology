4d144e4ecba7c77a8ca99c47c300d50c49b1f293
danielsab0001 <DSAB0001@ce.pucmm.edu.do>
2026-05-26 13:37:10 -0400

    Creación de repositorio usuario

--- jordis/src/main/java/com/jordis/jordis/repository/UsuarioRepository.java
+++ jordis/src/main/java/com/jordis/jordis/repository/UsuarioRepository.java
@@ -1,4 +1,15 @@
 package com.jordis.jordis.repository;
 
-public class UsuarioRepository {
+import com.jordis.jordis.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByNombreAndActivoTrue(String nombre);
+
+    boolean existsByNombre(String nombre);
 }
