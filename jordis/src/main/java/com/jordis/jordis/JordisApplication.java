ef7ab2ddfe7bf634542369d148700f1e399f91dc
danielsab0001 <DSAB0001@ce.pucmm.edu.do>
2026-05-26 12:52:23 -0400

    Creación de proyecto con librerías necesarias

---
+++ jordis/src/main/java/com/jordis/jordis/JordisApplication.java
@@ -0,0 +1,13 @@
+package com.jordis.jordis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JordisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JordisApplication.class, args);
+    }
+
+}
---
+++ jordis/src/test/java/com/jordis/jordis/JordisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordis.jordis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JordisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
